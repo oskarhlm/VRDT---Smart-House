bea226c2c474dd810521910e5b9543e83a6db650
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2023-02-20 01:20:26 +0100

    Create bash script for code generation

---
+++ python/generate_grpc_code.sh
@@ -0,0 +1,5 @@
+proto_folder=../protos
+proto_path=$proto_folder/netatmo.proto
+output_path=.
+
+python -m grpc_tools.protoc -I../protos --python_out=$output_path --pyi_out=$output_path --grpc_python_out=$output_path $proto_path
